b3718b656d60dd0e36d0b936dc5b8865aa2b076c
PetarSimonovic <petar@simonovic.com>
2020-12-13 10:39:12 +0000

    Interactive menu: add and print

--- directory.rb
+++ directory.rb
@@ -1,3 +1,28 @@
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    # 2. read the input and save it into a variable
+    # 3. do what the user has asked
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+      when "1"
+        students = input_students
+      when "2"
+        print_header
+        print_students(students)
+        print_footer(students)
+      when "9"
+        exit
+      else
+        puts "I don't know what you mean, try again"
+      end
+    end
+  end
+
 def input_students
   puts "Please enter the names and the cohorts of the students"
   puts "To finish, just hit return twice"
@@ -24,31 +49,18 @@ end
 
 #print list of students
 def print_header
-  puts "The students of Villains Academy"
+  puts "\nThe students of Villains Academy"
   puts "-------------"
 end
 
 def print_students(students)
-  puts "Search by letter or press return for full list"
-  alphabet = gets.chomp
-  if alphabet.empty?
-    students.each_with_index do |student, index|
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end
-  elsif !alphabet.empty?
-    students.each_with_index do |student, index|
-      if student[:name][0] == alphabet.upcase
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-      end
-    end
+  students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(student_number)
-  puts "Overall, we have #{student_number.count} great students"
+  puts "Overall, we have #{student_number.count} great students\n"
 end
 
-students = input_students
-print_header
-print_students(students)
-print_footer(students)
+interactive_menu
